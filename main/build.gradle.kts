8c0930f0045a7576d8d2b9f1fecf7955ac8a2cf1
Aleksei Vinogradov <coffeewithoutlemon@gmail.com>
2024-10-05 15:40:05 +0700

    Bottom menu implementation

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -43,8 +43,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.navigation.fragment)
-    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
