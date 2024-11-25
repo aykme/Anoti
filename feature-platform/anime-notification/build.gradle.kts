e1a44dc563141e13d678250f9cb42f1a4300f824
Aleksei Vinogradov <coffeewithoutlemon@gmail.com>
2024-11-25 18:16:47 +0700

    sdk and dependencies up

--- feature-platform/anime-notification/build.gradle.kts
+++ feature-platform/anime-notification/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.alekseivinogradov.anime_notification_platform"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 26
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
