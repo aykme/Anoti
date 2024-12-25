e05a366733cd67bb631f5f220373dd1ceda6207e
Aleksei Vinogradov <coffeewithoutlemon@gmail.com>
2024-12-25 19:55:34 +0700

    AnimeFavorites usecases tests implementation

--- feature-kmp/anime-favorites/src/test/kotlin/com/alekseivinogradov/anime_favorites/impl/domain/usecase/FetchAnimeDetailsByIdUsecaseTest.kt
+++ feature-kmp/anime-favorites/src/test/kotlin/com/alekseivinogradov/anime_favorites/impl/domain/usecase/FetchAnimeDetailsByIdUsecaseTest.kt
@@ -6,34 +6,24 @@ import com.alekseivinogradov.anime_favorites.impl.data.source.DesiredResult
 import com.alekseivinogradov.network.api.domain.model.CallResult
 import kotlinx.coroutines.test.runTest
 import kotlin.random.Random
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertTrue
-import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
 class FetchAnimeDetailsByIdUsecaseTest {
-    private val maxDelay = 600000 //10 minutes
-    private var randomDelay: Duration? = null
-    private var randomId: Int? = null
-    private var source: AnimeFavoritesSource? = null
-    private var usecase: FetchAnimeDetailsByIdUsecase? = null
-
-    @BeforeTest
-    fun setup() {
-        randomDelay = Random.nextInt(maxDelay).milliseconds
-        randomId = Random.nextInt(Int.MAX_VALUE)
-    }
+    private val maxDelay = 60000 //1 minute
+    private lateinit var source: AnimeFavoritesSource
+    private lateinit var usecase: FetchAnimeDetailsByIdUsecase
 
     @Test
     fun testFetchAnimeDetailsByIdUsecaseSuccessResult() = runTest {
         //Given
         initSourceAndUsecase(desiredResult = DesiredResult.SUCCESS)
-        val expectedResult = source!!.getItemById(randomId!!)
+        val randomId = getRandomId()
+        val expectedResult = source.getItemById(randomId)
 
         //When
-        val result = usecase!!.execute(randomId!!)
+        val result = usecase.execute(randomId)
 
         //Then
         assertTrue {
@@ -47,10 +37,11 @@ class FetchAnimeDetailsByIdUsecaseTest {
     fun testFetchAnimeDetailsByIdUsecaseHttpErrorResult() = runTest {
         //Given
         initSourceAndUsecase(desiredResult = DesiredResult.HTTP_ERROR)
-        val expectedResult = source!!.getItemById(randomId!!)
+        val randomId = getRandomId()
+        val expectedResult = source.getItemById(randomId)
 
         //When
-        val result = usecase!!.execute(randomId!!)
+        val result = usecase.execute(randomId)
 
         //Then
         assertTrue {
@@ -65,10 +56,11 @@ class FetchAnimeDetailsByIdUsecaseTest {
     fun testFetchAnimeDetailsByIdUsecaseOtherErrorResult() = runTest {
         //Given
         initSourceAndUsecase(desiredResult = DesiredResult.OTHER_ERROR)
-        val expectedResult = source!!.getItemById(randomId!!)
+        val randomId = getRandomId()
+        val expectedResult = source.getItemById(randomId)
 
         //When
-        val result = usecase!!.execute(randomId!!)
+        val result = usecase.execute(randomId)
 
         //Then
         assertTrue {
@@ -81,16 +73,10 @@ class FetchAnimeDetailsByIdUsecaseTest {
     private fun initSourceAndUsecase(desiredResult: DesiredResult) {
         source = AnimeFavoritesSourceImplFake(
             desiredResult = desiredResult,
-            desiredDelay = randomDelay!!
+            desiredDelay = Random.nextInt(maxDelay).milliseconds
         )
-        usecase = FetchAnimeDetailsByIdUsecase(source!!)
+        usecase = FetchAnimeDetailsByIdUsecase(source)
     }
 
-    @AfterTest
-    fun cleanUp() {
-        randomDelay = null
-        randomId = null
-        source = null
-        usecase = null
-    }
+    private fun getRandomId() = Random.nextInt(Int.MAX_VALUE)
 }
