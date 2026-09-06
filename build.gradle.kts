fc56fdbd52121c3d566cfd20bc2b3a9497640d2f
Aleksei V <coffeewithoutlemon@gmail.com>
2026-09-07 06:54:25 +0700

    chore: remove the unused androidLibrary plugin alias

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.detekt) apply false
